c4f7302efc313bfb7db89a6e71ae7ba3253ba0fa
MenesiZoltan <colos.menesi@gmail.com>
2019-05-02 13:02:35 +0200

    Refactored event handler functions, updated visual design and element alignment.

---
+++ src/ProcessNote.UI/EventHandler.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProcessNote.CurrentProcess;
+using ProcessNote.Collector;
+using System.Windows.Controls;
+
+namespace ProcessNote.UI
+{
+    class EventHandler
+    {
+        private ProcessAggregator processes = new ProcessAggregator();
+        private void VisualizeProcesses(DataGrid DataGridXML)
+        {
+            foreach (CurrentllyRunningProcess process in processes.getProcesses())
+            {
+                DataGridXML.Items.Add(process);
+            }
+
+        }
+
+        public void ShowProcessDetails(ListBox ProcessDetailsXML, TextBox ProcessNote, DataGrid DataGridXML)
+        {
+            try
+            {
+                ProcessDetailsXML.Items.Clear();
+                ProcessNote.Text = String.Empty;
+                CurrentllyRunningProcess process = (CurrentllyRunningProcess)DataGridXML.SelectedItem;
+                ProcessDetailsXML.Items.Add(process.name + "\n" + process.cpuUsage + "\n" + process.memoryUsage + "\n" + process.startTime + "\n" + process.runTime + "\n");
+                ProcessNote.Text = process.note;
+            }
+            catch (System.NullReferenceException exception)
+            {
+                Console.WriteLine(exception.ToString());
+            }
+
+        }
+
+        public void PopulateTableOnLoad(DataGrid DataGridXML)
+        {
+            processes.getAllRunningProcess();
+            DataGridXML.IsReadOnly = true;
+            VisualizeProcesses(DataGridXML);
+        }
+
+        public void SearchOnWeb(DataGrid DataGridXML)
+        {
+            CurrentllyRunningProcess process = (CurrentllyRunningProcess)DataGridXML.SelectedItem;
+            try
+            {
+                string searchQuery = process.name;
+                System.Diagnostics.Process.Start("https://www.google.com/search?q=" + Uri.EscapeDataString(searchQuery));
+            }
+            catch (System.NullReferenceException exception)
+            {
+                Console.WriteLine(exception.ToString());
+            }
+        }
+
+        public void SaveComment(DataGrid DataGridXML, TextBox ProcessNote)
+        {
+            CurrentllyRunningProcess process = (CurrentllyRunningProcess)DataGridXML.SelectedItem;
+            string note = ProcessNote.Text;
+            process.addNote(note);
+        }
+
+        public void RefreshOnDoubleClick(DataGrid DataGridXML, TextBox ProcessNote, ListBox ProcessDetailsXML)
+        {
+            CurrentllyRunningProcess process = (CurrentllyRunningProcess)DataGridXML.SelectedItem;
+            ProcessDetailsXML.Items.Clear();
+            processes.EmptyContainer();
+            DataGridXML.Items.Clear();
+            processes.getAllRunningProcess();
+            VisualizeProcesses(DataGridXML);
+            try
+            {
+                ProcessDetailsXML.Items.Add(process.name + "\n" + process.cpuUsage + "\n" + process.memoryUsage + "\n" + process.startTime + "\n" + process.runTime + "\n");
+            }
+            catch (System.NullReferenceException c)
+            {
+                Console.WriteLine(c.ToString());
+            }
+        }
+
+    }
+
+}
--- src/ProcessNote.UI/MainWindow.xaml.cs
+++ src/ProcessNote.UI/MainWindow.xaml.cs
@@ -9,96 +9,37 @@ namespace ProcessNote.UI
     
     public partial class MainWindow : Window
     {
-        ProcessAggregator processes = new ProcessAggregator();
+        
+        private EventHandler eventHandler = new EventHandler();
 
         public MainWindow()
         {
             InitializeComponent();   
         }
 
-
-        public void VisualizeProcesses()
-        {
-            foreach (CurrentllyRunningProcess process in processes.getProcesses())
-            {
-                DataGridXML.Items.Add(process);
-            }
-
-        }
-
         private void DataGridXML_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
         {
-            try
-            {
-                ProcessDetailsXML.Items.Clear();
-                ProcessNote.Text = String.Empty;
-                CurrentllyRunningProcess process = (CurrentllyRunningProcess)DataGridXML.SelectedItem;
-                ProcessDetailsXML.Items.Add(process.name + "\n" +  process.cpuUsage+ "\n" + process.memoryUsage + "\n" + process.startTime + "\n" + process.runTime + "\n");
-                ProcessNote.Text = process.note;
-            }
-            catch (System.NullReferenceException exception)
-            {
-                Console.WriteLine(exception.ToString());
-            }
-
-
+            eventHandler.ShowProcessDetails(ProcessDetailsXML, ProcessNote, DataGridXML);
         }
 
-       
         private void DataGridXML_Loaded(object sender, RoutedEventArgs e)
         {
-            processes.getAllRunningProcess();
-            DataGridXML.IsReadOnly = true;
-            VisualizeProcesses();
+            eventHandler.PopulateTableOnLoad(DataGridXML);
         }
 
         private void SearchButton_Click(object sender, RoutedEventArgs e)
         {
-
-            CurrentllyRunningProcess process = (CurrentllyRunningProcess)DataGridXML.SelectedItem;
-
-            try
-            {
-                string searchQuery = process.name;
-                System.Diagnostics.Process.Start("https://www.google.com/search?q=" + Uri.EscapeDataString(searchQuery));
-
-            } catch (System.NullReferenceException exception)
-            {
-                Console.WriteLine(exception.ToString());
-            }
-
-
+            eventHandler.SearchOnWeb(DataGridXML);
         }
-
-
-        /*Eventhandler for Save Comment button*/
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
-            CurrentllyRunningProcess process = (CurrentllyRunningProcess)DataGridXML.SelectedItem;
-            string note = ProcessNote.Text;
-            process.addNote(note);
-
+            eventHandler.SaveComment(DataGridXML, ProcessNote);
         }
 
         private void DataGridXML_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-            CurrentllyRunningProcess process = (CurrentllyRunningProcess)DataGridXML.SelectedItem;
-            ProcessDetailsXML.Items.Clear();
-            processes.EmptyContainer();
-            DataGridXML.Items.Clear();
-            processes.getAllRunningProcess();
-            VisualizeProcesses();
-
-            try
-            {
-                ProcessDetailsXML.Items.Add(process.name + "\n" + process.cpuUsage + "\n" + process.memoryUsage + "\n" + process.startTime + "\n" + process.runTime + "\n");
-            }
-            catch (System.NullReferenceException c)
-            {
-                Console.WriteLine(c.ToString());
-            }
+            eventHandler.RefreshOnDoubleClick(DataGridXML, ProcessNote, ProcessDetailsXML);
         }
     }
 }
